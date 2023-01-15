aed7666ce051cf3cffe4dc069ead924652f5c7e4
askar <mohamedaskar476@gmail.com>
2023-01-15 12:40:50 +0530

    Implemented Pagination successfully

--- README.md
+++ README.md
@@ -4,8 +4,12 @@ Implementing Pagination using React
 
 ### Objective
 - Get Data and store as State
-- Show 10 sets of data per page
+- Show 9 sets of data per page
 - Prev and Next buttons
 
 ### Stack
 - React
+
+### Features
+- Pagination
+- User can choose no. of products per page (to be added)
--- src/App.js
+++ src/App.js
@@ -17,22 +17,54 @@ function App() {
     setProducts(data.products)
   }
 
-  // console.log(products);
+  // console.log(Math.ceil(100/9));
 
 
   return (
-    <div className="products">
-      {
-        products && products.map(product => {
-          return (
-            <div key={product.id} className='product__single'>
-              <img src={product.thumbnail} alt={product.title}/>
-              <h3 >{product.title}</h3>
-            </div>
-          )
-        })
-      } 
-    </div>
+    <>
+      <div className="products">
+        {
+          // 9 Products per page
+          // For page - 1 (First Index = 0, Last Index = 9)
+          // First Index -  page * 9 - 9  =   1 * 9 - 9   = 0
+          // Last index -   page * 9      =   1 * 9       = 9
+          // products.slice(0,9) - for page no. 1 and so on.
+          products && products.slice(page * 9 - 9, page * 9).map(product => {
+            return (
+              <div key={product.id} className='product__single'>
+                <img src={product.thumbnail} alt={product.title}/>
+                <h3 >{product.title}</h3>
+              </div>
+            )
+          })
+        } 
+      </div>
+      <div className='pagination'>
+        {
+          
+        }
+        <button onClick={page !== 1 ? () => setPage(page - 1) : null}>Prev</button>
+        {
+          /* 
+            Total products = 100, products per page = 9
+            No. of pages =  Math.ceil(Total_products/products_per_page) = 100/9 = 12
+            Create an empty array of 12 elements to generate page numbers
+          */
+          [...Array(Math.ceil(100/9))].map((_,i) => {
+            return (
+              <button
+                key={i}
+                className={page === i+1 ? "current__page" : ""}
+                onClick={() => setPage(i+1)}
+              >
+                {i+1}
+              </button>
+            )
+          })
+        }
+        <button onClick={page !== Math.ceil(100/9) ? () => setPage(page + 1) : null}>Next</button>
+      </div>
+    </>
   );
 }
 
